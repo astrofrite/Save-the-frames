90bdfa3b119bc72dc5ca2eacda82480c4bd872ad
astrofrite <felix.manson@icloud.com>
2020-05-01 21:18:21 +0200

    added a new tileset

--- Tileset/Tile set 1.tsx
+++ Tileset/Tile set 1.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="Tile set 1" tilewidth="32" tileheight="32" tilecount="128" columns="16">
- <image source="../Graphics/Tile set 1.png" width="512" height="256"/>
+<tileset version="1.2" tiledversion="1.3.4" name="Tile set 1" tilewidth="32" tileheight="32" tilecount="35" columns="5">
+ <image source="../Graphics/Tile set 1.png" width="160" height="224"/>
  <terraintypes>
   <terrain name="Nouveau terrain" tile="2"/>
   <terrain name="Nouveau terrain" tile="2"/>
@@ -21,36 +21,57 @@
    <object id="1" x="-0.181818" y="-0.181818" width="31.8182" height="32.3636"/>
   </objectgroup>
  </tile>
- <tile id="16" terrain="1,1,,">
+ <tile id="5" terrain="1,1,,">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="31.8182" height="32.3636"/>
   </objectgroup>
  </tile>
- <tile id="17" terrain="1,1,,">
+ <tile id="6" terrain="1,1,,">
   <objectgroup draworder="index" id="2">
    <object id="1" x="-0.181818" y="0" width="32.1818" height="32"/>
   </objectgroup>
  </tile>
- <tile id="18" terrain="1,1,,">
+ <tile id="7" terrain="1,1,,">
   <objectgroup draworder="index" id="2">
    <object id="2" x="0" y="0" width="32" height="32.1818"/>
   </objectgroup>
  </tile>
- <tile id="32">
+ <tile id="10">
   <objectgroup draworder="index" id="2">
    <object id="1" x="-0.0909091" y="-0.0909091">
     <polygon points="0,0 32.0909,0.0909091 32.1818,32.0909 0.0909091,32"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="33">
+ <tile id="11">
   <objectgroup draworder="index" id="2">
    <object id="1" x="-0.363636" y="0" width="32.1818" height="31.8182"/>
   </objectgroup>
  </tile>
- <tile id="34">
+ <tile id="12">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="-0.181818" width="31.8182" height="32.1818"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31.6508" y="15.8254">
+    <polygon points="0.26523,-0.08841 -31.6508,14.0572 -31.6508,16.179 0.17682,16.179"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31.7392" y="-0.08841">
+    <polygon points="0.08841,-0.08841 -4.15527,1.06092 -31.916,14.0572 -31.6508,32.0928 0.26523,31.916"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-0.17682">
+    <polygon points="0,0 32.0044,30.5899 32.0044,32.0928 0.17682,32.1812"/>
+   </object>
   </objectgroup>
  </tile>
  <wangsets>
