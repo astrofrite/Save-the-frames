9caeb19ef28e5f8e305ae8d7c3be3ff7e10ac7ed
Félix Manson <pousin1er@gmail.com>
2020-05-01 22:40:40 +0200

    Game properly work now !

--- Actor/Actor.gd
+++ Actor/Actor.gd
@@ -2,13 +2,9 @@ extends KinematicBody2D
 class_name Actor 
 
 export var gravity: = 4000.0
-
+const FLOOR_NORMAL = Vector2.UP
 export var speed: = Vector2(300.0, 1000.0)
 
 var velocity: = Vector2.ZERO
 
-func _physics_process(delta) -> void:
-	velocity.y = gravity * delta
-	 # a bouger dans les scripts des acteur
-	
-	
+
--- Tileset/Tile set ground 1.tsx
+++ Tileset/Tile set ground 1.tsx
@@ -1,37 +1,32 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.4" name="Tile set 1" tilewidth="32" tileheight="32" tilecount="35" columns="5">
  <image source="../Graphics/Tile set 1.png" width="160" height="224"/>
- <terraintypes>
-  <terrain name="Nouveau terrain" tile="2"/>
-  <terrain name="Nouveau terrain" tile="2"/>
-  <terrain name="Nouveau terrain" tile="2"/>
- </terraintypes>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32.1818"/>
   </objectgroup>
  </tile>
- <tile id="1" terrain="2,2,2,2">
+ <tile id="1">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="31.6364" height="32"/>
   </objectgroup>
  </tile>
- <tile id="2" terrain="1,1,1,1">
+ <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-0.181818" y="-0.181818" width="31.8182" height="32.3636"/>
+   <object id="2" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
- <tile id="5" terrain="1,1,,">
+ <tile id="5">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="31.8182" height="32.3636"/>
   </objectgroup>
  </tile>
- <tile id="6" terrain="1,1,,">
+ <tile id="6">
   <objectgroup draworder="index" id="2">
    <object id="1" x="-0.181818" y="0" width="32.1818" height="32"/>
   </objectgroup>
  </tile>
- <tile id="7" terrain="1,1,,">
+ <tile id="7">
   <objectgroup draworder="index" id="2">
    <object id="2" x="0" y="0" width="32" height="32.1818"/>
   </objectgroup>
@@ -53,6 +48,20 @@
    <object id="1" x="0" y="-0.181818" width="31.8182" height="32.1818"/>
   </objectgroup>
  </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="-0.125,-0.125 31.8125,13.9375 32,32 0,32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.0625" y="16">
+    <polygon points="0,0 32.0625,16 0.0625,16"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="17">
   <objectgroup draworder="index" id="2">
    <object id="1" x="31.6508" y="15.8254">
@@ -63,8 +72,18 @@
  <tile id="18">
   <objectgroup draworder="index" id="2">
    <object id="1" x="31.7392" y="-0.08841">
-    <polygon points="0.08841,-0.08841 -4.15527,1.06092 -31.916,14.0572 -31.6508,32.0928 0.26523,31.916"/>
+    <polygon points="0.08841,-0.08841 -31.916,14.0572 -31.6508,32.0928 0.26523,31.916"/>
    </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <tile id="26">
@@ -72,6 +91,23 @@
    <object id="1" x="0" y="-0.17682">
     <polygon points="0,0 32.0044,30.5899 32.0044,32.0928 0.17682,32.1812"/>
    </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0">
+    <polygon points="0,0 -32,32 0,32 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
  <wangsets>
