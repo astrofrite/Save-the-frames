2536cc8f31f4d85bd912184d5275d1bee7a2eace
Félix Manson <pousin1er@gmail.com>
2020-05-10 20:41:44 +0200

    V4
    Adding following camera, gravity control, gravity killtest, and a proper animated sprite !

--- Actor/player.gd
+++ Actor/player.gd
@@ -1,10 +1,24 @@
 extends Actor
 
+var gravstate = 2
+
+func _ready():
+	get_node("Camera2D/Label").hide()
+
 func _physics_process(delta):
+	var gravity_ = _grav(get_gravity_state())
 	var is_jump_canceled = Input.is_action_just_released("ui_up")
 	var direction = get_direction()
-	velocity = calculate_move_velocity(velocity,direction,speed,is_jump_canceled)
+	velocity = calculate_move_velocity(velocity, direction, speed, gravity_, is_jump_canceled)
 	velocity = move_and_slide(velocity,FLOOR_NORMAL)
+	if velocity.x != 0 :
+		var sprite = $AnimatedSprite
+		sprite.play()
+		sprite.flip_v = false
+		sprite.flip_h = velocity.x < 0
+	else :
+		$AnimatedSprite.stop()
+		$AnimatedSprite.set_frame(0)
 	
 	
 func get_direction() -> Vector2:
@@ -17,11 +31,12 @@ func calculate_move_velocity(
 	linear_velocity: Vector2,
 	direction : Vector2,
 	speed: Vector2,
+	_grav: float,
 	jump_canceled: bool
 	) -> Vector2:
 		var new_velocity: = linear_velocity
 		new_velocity.x = speed.x * direction.x
-		new_velocity.y += gravity * get_physics_process_delta_time()
+		new_velocity.y += _grav * get_physics_process_delta_time()
 		if direction.y == -1.0 :
 			new_velocity.y = speed.y * direction.y
 			
@@ -39,3 +54,50 @@ func can_jump() -> bool:
 		return true
 	else :
 		return false
+
+func get_gravity_state() -> int:
+	if Input.is_action_pressed("gravity_1"):
+		gravstate=1
+	elif Input.is_action_pressed("gravity_2"):
+		gravstate=2
+	elif Input.is_action_pressed("gravity_3"):
+		gravstate=3
+	elif Input.is_action_pressed("gravity_4"):
+		gravstate=4
+	return gravstate
+	
+
+func _grav(gravity_state : int) -> float :
+	var _grav
+	if gravity_state == 1 :
+		_grav = 200
+	if gravity_state == 2:
+		_grav = 400
+	if gravity_state == 3 :
+		_grav = 800
+	if gravity_state == 4 :
+		_grav = 1200
+	return _grav
+
+
+func _on_speedtest_test():
+	if gravstate == 1:
+		pass
+	else :
+		kill("gravity")
+		pass
+	
+	
+
+func kill(reas :String):
+	get_node("AnimatedSprite").hide()
+	var messagelabel = get_node("Camera2D/Label")
+	if reas == "gravity" :
+		messagelabel.text = "Killed by gravity"
+		messagelabel.show()
+	else :
+		messagelabel.text = "Killed by ??"
+		messagelabel.show()
+	
+	set_physics_process(false)
+	
